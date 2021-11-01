3460fc96c8111daae0f2589391395f14292668dc
Nikita <simoyanov93@gmail.com>
2021-11-01 12:16:34 +0300

    fix range slider
    add js to control value line

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,6 +1,29 @@
 $( document ).ready(function() {
     console.log( "ready!" );
 
+const rangeInputs = document.querySelectorAll('input[type="range"]')
+const numberInput = document.querySelector('input[type="number"]')
+
+function handleInputChange(e) {
+  let target = e.target
+  if (e.target.type !== 'range') {
+    target = document.getElementById('range')
+  } 
+  const min = target.min
+  const max = target.max
+  const val = target.value
+  
+  target.style.backgroundSize = (val - min) * 100 / (max - min) + '% 100%'
+}  
+
+rangeInputs.forEach(input => {
+    
+  let min = input.min
+  let max = input.max
+  let val = input.value
+  input.style.backgroundSize = (val - min) * 100 / (max - min) + '% 100%'    
+  input.addEventListener('input', handleInputChange)
+})
 
     
 });
